c9cc160fa0b184b9ac234ff722d3a2e01cf61afa
SHUBHAM KUMAR <shubhamkumar@SHUBHAMs-MacBook-Air.local>
2023-07-20 00:00:26 +0530

    handle submit with handle blur

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,19 +1,63 @@
 import React from "react";
+import { useFormik } from "formik";
+
+const initialValues = {
+  name: "",
+  email: "",
+  password: "",
+  confirm_password: "",
+};
+
+// function Form() {
+//   const formik = useFormik({
+//     initialValues: initialValues,
+//     onSubmit: (values) => {
+//       console.log("The values are : ", values);
+//     },
+//   });
+
+//   console.log(formik); //this will return an object with a lot of functions which we can check in the console and it also has an object named values which have all our input fields
 
 function Form() {
+  // This time we are destructuring the formik object so that we dont have to write Formik.values.name etc
+
+  // we can directly now write values.name
+  const { values, errors, handleBlur, handleChange, handleSubmit } = useFormik({
+    initialValues: initialValues,
+    onSubmit: (values) => {
+      console.log("The values are : ", values);
+    },
+  });
+
   return (
     <>
       <h1>Welcome !</h1>
       <h3>This is just a test form to test validation </h3>
       <div>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div>
             <label>Name</label>
-            <input type="text" name="name" id="name" placeholder="Name" />
+            <input
+              type="text"
+              name="name"
+              id="name"
+              placeholder="Name"
+              value={values.name}
+              onChange={handleChange}
+              onBlur={handleBlur}
+            />
           </div>
           <div>
             <label>Email</label>
-            <input type="email" name="email" id="email" placeholder="Email" />
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Email"
+              value={values.email}
+              onChange={handleChange}
+              onBlur={handleBlur}
+            />
           </div>
           <div>
             <label>Password</label>
@@ -22,6 +66,9 @@ function Form() {
               name="password"
               id="password"
               placeholder="Password"
+              value={values.password}
+              onChange={handleChange}
+              onBlur={handleBlur}
             />
           </div>
           <div>
@@ -31,6 +78,9 @@ function Form() {
               name="confirm_password"
               id="confirm_password"
               placeholder="Confirm Password"
+              value={values.confirm_password}
+              onChange={handleChange}
+              onBlur={handleBlur}
             />
           </div>
           {/* <div>
